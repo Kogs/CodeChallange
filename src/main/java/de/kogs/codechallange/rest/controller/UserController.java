092220f474367212d988e2df4d892ee0b687b701
Marcel Vogel <marcel.vogel95@gmx.de>
2021-07-14 17:55:13 +0200

    cleanup imports

--- src/main/java/de/kogs/codechallange/rest/controller/UserController.java
+++ src/main/java/de/kogs/codechallange/rest/controller/UserController.java
@@ -2,11 +2,9 @@ package de.kogs.codechallange.rest.controller;
 
 import java.util.Collection;
 
-import org.hibernate.annotations.common.util.impl.Log_.logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 import de.kogs.codechallange.data.User;
 import de.kogs.codechallange.rest.dto.UserDto;
 import de.kogs.codechallange.service.UserService;
-import jdk.internal.jline.internal.Log;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
